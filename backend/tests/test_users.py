ad3421c77e51ceca6f0979909ca7da92b0882c1e
rixka <rixka@outlook.com>
2017-02-09 16:36:24 +0000

    [add] more user tests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -3,22 +3,71 @@ import pytest
 from lib.users import User
 from common import DynamoSystemTest, invoke_action
 
+username = 'apex-jump-bot'
+email = 'foo@bar.com'
 
-class TestNewUser(DynamoSystemTest):
+class TestUsers(DynamoSystemTest):
     mocked = True
     models = [ User ]
 
-    @classmethod
-    def setup_class_custom(cls):
-        cls.action = 'new-user'
-        cls.username = 'apex-jump-bot'
+    class TestNewUser(object):
+        @classmethod
+        def setup_class(cls):
+            cls.action = 'new-user'
 
-    def setup(self):
-        self.body = {
-            'username': self.username,
-            'email': 'foo@bar.com'
-        }
+        def setup(self):
+            self.body = {
+                'username': username,
+                'email': email
+            }
 
-    def test_successful(self):
-        user = invoke_action(self.action, self.body)
-        assert User.item_exists(user.id)
+        def test_successful(self):
+            res = invoke_action(self.action, self.body)
+            assert User.item_exists(res['id'])
+
+        @pytest.mark.skip(reason='Not implemented yet')
+        def test_conflict(self):
+            User.new(self.body)
+
+            with pytest.raises(Exception) as e:
+                invoke_action(self.action, self.body)
+            assert 'Conflict' in e.value.message
+
+        @pytest.mark.skip(reason='Not implemented yet')
+        def test_bad_request(self):
+            with pytest.raises(Exception) as e:
+                invoke_action(self.action)
+            assert 'Bad Request' in e.value.message
+
+
+    class TestGetUser(object):
+        @classmethod
+        def setup_class(cls):
+            cls.action = 'get-user'
+
+            cls.user = User.new({
+                'username': username,
+                'email': email
+            })
+
+        def setup(self):
+            self.body = {
+                'id': self.user['id']
+            }
+
+        def test_successful(self):
+            res = invoke_action(self.action, self.body)
+            assert res == self.user
+
+        def test_not_found(self):
+            self.body['id'] = 'foobar'
+
+            with pytest.raises(Exception) as e:
+                invoke_action(self.action, self.body)
+            assert 'Not Found' in e.value.message
+
+        @pytest.mark.skip(reason='Not implemented yet')
+        def test_bad_request(self):
+            with pytest.raises(Exception) as e:
+                invoke_action(self.action)
+            assert 'Bad Request' in e.value.message
